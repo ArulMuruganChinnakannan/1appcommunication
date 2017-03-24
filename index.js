daa5bd531195dc1bd4ad30a4fe3992ea29afaaec
ArulMuruganChinnakannan <kannanarulmurugan09@gmail.com>
2017-03-24 14:35:27 -0700

    Update index.js

--- index.js
+++ index.js
@@ -1,30 +1,18 @@
-var cool = require('cool-ascii-faces');
-var express = require('express');
-var app = express();
+var app = require('express')();
 var http = require('http').Server(app);
 var io = require('socket.io')(http);
-var port = process.env.PORT || 5000;
-app.set('port', port);
+var port = process.env.PORT || 3000;
 
-app.use(express.static(__dirname + '/public'));
-
-// views is directory for all template files
-app.set('views', __dirname + '/views');
-app.set('view engine', 'ejs');
-
-app.get('/', function(request, response) {
-  response.render('pages/index')
+app.get('/', function(req, res){
+  res.sendFile(__dirname + '/index.html');
 });
+
 io.on('connection', function(socket){
   socket.on('chat message', function(msg){
     io.emit('chat message', msg);
   });
 });
-app.get('/cool', function(request, response) {
-  response.send(cool());
-});
 
-app.listen(app.get('port'), function() {
-  console.log('Node app is running on port', app.get('port'));
+http.listen(port, function(){
+  console.log('listening on *:' + port);
 });
-
